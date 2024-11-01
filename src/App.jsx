be2a6f328fb17a4883f8104e4280a8e71e5bb584
Abdel Sayedahmed <abdelrahman.s2003@icloud.com>
2024-11-01 17:32:22 -0400

    added footer renamed NavBar to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import Home from "./components/Home.jsx";
-import Nav from "./components/LandingPage.jsx";
 import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
+import LandingPage from "./components/LandingPage.jsx";
 import Projects from "./components/Projects.jsx";
 
 export default function App() {
+  const location = useLocation();
+
   return (
     <>
+      {location.pathname !== "/" && <Header />}
       <Routes>
-        <Route path="/" element={<Nav />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Projects" element={<Projects />} />
       </Routes>
+      {location.pathname !== "/" && <Footer />}
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function LandingPage() {
+export default function Header() {
   return (
-    <div className="bg-black text-white flex flex-col items-center min-h-screen bg-[url('/mountain-range.png')] bg-[70%_auto] bg-no-repeat bg-center">
-      <img src="./Logo.png" alt="Abdels logo" className="mt-[5%]" />
-      <div className="text-[30px] text-center font-semibold mt-[10%]">
-        <p className="bg-white bg-opacity-20 px-[5px]">Practice beats talents</p>
-        <p className="bg-white bg-opacity-20 px-[5px] italic w-max mx-auto">- unknown</p>
-      </div>
-      <div className="flex gap-5 text-[24px]  mt-[10%]">
+    <div className="bg-black h-auto text-white flex items-center p-[20px] justify-between">
+      <Link to="/" className="flex flex-col text-center">
+        <h4 className="text-[22px]">Abdelrahman Sayedahmed</h4>
+        <h6>-Software Developer-</h6>
+      </Link>
+      <div className="flex items-center gap-5">
         <Link
           to="/Home"
           className="border border-[1px] px-[20px] py-[5px] hover:bg-white hover:bg-opacity-50"
