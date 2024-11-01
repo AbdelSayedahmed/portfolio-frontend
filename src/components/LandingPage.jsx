6bd79900f27f5884b531bc90c0d0e0d851902b2f
Abdel Sayedahmed <abdelrahman.s2003@icloud.com>
2024-11-01 16:11:06 -0400

    landing main css complete pre-transition

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="bg-black text-white flex flex-col justify-around items-center min-h-screen bg-[url('/mountain-range.png')] bg-[70%_auto] bg-no-repeat bg-center">
-      <img src="./Logo.png" alt="Abdels logo" />
-      <div>
-        <p>Practice beats talents</p>
-        <p>- unknown</p>
+    <div className="bg-black text-white flex flex-col items-center min-h-screen bg-[url('/mountain-range.png')] bg-[70%_auto] bg-no-repeat bg-center">
+      <img src="./Logo.png" alt="Abdels logo" className="mt-[5%]" />
+      <div className="text-[30px] text-center font-semibold mt-[10%]">
+        <p className="bg-white bg-opacity-20 px-[5px]">Practice beats talents</p>
+        <p className="bg-white bg-opacity-20 px-[5px] italic w-max mx-auto">- unknown</p>
       </div>
-      <div className="flex gap-5">
+      <div className="flex gap-5 text-[24px]  mt-[10%]">
         <Link
           to="/Home"
           className="border border-[1px] px-[20px] py-[5px] hover:bg-white hover:bg-opacity-50"
