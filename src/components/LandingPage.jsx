dbafdb863d28c97ec845d5fbf1bc6443fb14cd54
Abdel Sayedahmed <abdelrahman.s2003@icloud.com>
2024-11-22 13:11:38 -0500

    redirects

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,8 +3,8 @@ import Menu from "./Menu";
 
 export default function LandingPage() {
   return (
-    <div className="bg-black text-white flex flex-col items-center min-h-screen bg-[url('./mountain-range.png')] bg-[70%_auto] bg-no-repeat bg-center">
-      <img src="./logo.png" alt="Abdel's logo" className="mt-[5%]" />
+    <div className="bg-black text-white flex flex-col items-center min-h-screen bg-[url('./images/mountain-range.png')] bg-[70%_auto] bg-no-repeat bg-center">
+      <img src="./images/logo.png" alt="Abdel's logo" className="mt-[5%]" />
       <div className="text-[30px] text-center font-semibold mt-[10%]">
         <p className="bg-white bg-opacity-20 px-[5px]">
           Practice beats talents
