247557b117c47226f56fad1e38e57a54c16bf8af
Abdel Sayedahmed <abdelrahman.s2003@icloud.com>
2024-11-22 11:41:38 -0500

    contact added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div className="flex flex-col justify-center items-center p-10 m-20 border border-black border-2 rounded-[24px]">
+      <div>
+      <h1>Contact Me</h1>
+      <p>
+        Email Me:{" "}
+        <Link to="mailto:abdelrahman.s2003@icloud.com">
+          abdelrahman.s2003@icloud.com
+        </Link>
+      </p>
+      <p>
+        Call Me: <Link to="tel:+16318294295">+1 (631) 829-4295</Link>
+      </p>
+      </div>
+    </div>
+  );
 }
