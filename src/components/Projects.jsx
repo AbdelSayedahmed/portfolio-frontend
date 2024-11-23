35c61ff8629e6d0d35673162b4bd8ecf530ab610
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-11-23 12:26:45 -0500

    redid projects view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,55 +17,31 @@ export default function Projects() {
     fetchProjects();
   }, []);
 
-  const samlpleProject = {
-    backend_link: "https://github.com/ayoublos/Recipe-Project-BackEnd",
-    created_at: "2024-11-03T21:31:55.998Z",
-    deployed_link: "https://culinarycompass.netlify.app/",
-    description:
-      "Culinary Compass is a web application designed to provide an extensive collection of recipes from around the world. Users can browse, contribute, and edit recipes while engaging with a community of food enthusiasts.",
-    frontend_link:
-      "https://github.com/AbdelSayedahmed/AbdelSayedahmed-Recipe-Project-FrontEnd",
+  const sampleProject = {
     id: 1,
+    name: "iHelp",
+    description:
+      "iHelp is a dynamic platform designed to bridge the gap between community organizations and teen volunteers, fostering meaningful connections while making volunteering an engaging and rewarding experience. Tailored to address the challenges non-profits face in recruiting and managing volunteers, iHelp provides two distinct interfaces, Administrative Side and the Volunteer Side.",
+    frontendLink: [
+      "https://github.com/anthonymartz17/ihelp_frontend_organization",
+      "https://github.com/anthonymartz17/ihelp-volunteers-frontend",
+    ],
+    backendLink: ["https://github.com/AbdelSayedahmed/ihelp-backend"],
+    deployedLink: [
+      "https://ihelp-org.netlify.app/login",
+      "https://ihelp-demo.netlify.app/login",
+    ],
     image_url:
-      "https://culinarycompass.netlify.app/app-logos/culinary-compass-logo.png",
-    name: "Recipe Project",
-    updated_at: "2024-11-03T21:31:55.998Z",
+      "https://github.com/anthonymartz17/ihelp_frontend_organization/blob/main/src/assets/logo/white_bg_ihelp_logo.png?raw=true",
   };
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <h1 className="text-[36px] font-bold">Projects</h1>
-      <div className="flex flex-wrap justify-center">
+      <div className="flex flex-wrap justify-center items-center gap-4">
         {projects.map((project) => (
-          <div
-            key={project.id}
-            className="flex w-1/3 border border-black rounded-[12px] p-2 m-4 gap-2"
-          >
-            <div className="flex just-center items-center w-1/2">
-              <img
-                src={project.image_url}
-                alt={project.name}
-                className="rounded-[12px] bg-[grey] w-[100%]"
-              />
-            </div>
-            <div className="w-1/2">
-              <h2 className="text-[20px] font-semibold">{project.name}</h2>
-              <p>{project.description}</p>
-              <div>
-                <h4 className="text-[20px] font-semibold">Links</h4>
-                <div className="flex flex-col gap-1 font-medium">
-                  <a href={project.frontend_link} target="_blank">
-                    Frontend
-                  </a>
-                  <a href={project.backend_link} target="_blank">
-                    Backend
-                  </a>
-                  <a href={project.deployed_link} target="_blank">
-                    Deployed
-                  </a>
-                </div>
-              </div>
-            </div>
+          <div key={project.id} className="w-[300px] h-[300px] p-4">
+            <img src={project.image_url} alt={project.name} className="w-full h-full" />
           </div>
         ))}
       </div>
